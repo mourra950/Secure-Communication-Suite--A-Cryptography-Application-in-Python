f5892c06c453f4ae16864277457bececa28ae8c9
Omar Mohamed Yousef <mourra24@gmail.com>
2024-04-23 19:18:54 +0200

    Fixed the RSA
    the error was caused by the naming of the variable used in the private key

--- RSA.py
+++ RSA.py
@@ -6,16 +6,17 @@ class RSA_Task():
 
     def __init__(self):  # ,plaintext_queue,ciphertext_queue,data
         super().__init__()
-        self.public_key, self.private_key = rsa.newkeys(1024)
+        self.rsa_public_key, self.rsa_private_key = rsa.newkeys(256)
 
     def RSA_Encrypt(self):
         plaintext = self.qt_text.toPlainText()
-        ciphertext = rsa.encrypt(plaintext.encode(), self.public_key)
+        ciphertext = rsa.encrypt(plaintext.encode(), self.rsa_public_key)
+        
         self.save_binary(ciphertext)
         self.qt_cipher.setText(str(ciphertext))
 
     def RSA_Decrypt(self):
         plaintext = self.read_binary()
-        outtext = rsa.decrypt(plaintext, self.private_key)
+        outtext = rsa.decrypt(plaintext, self.rsa_private_key)
         outtext = outtext.decode()
         self.qt_cipher.setText("Decrypted RSA is '"+outtext+"'")
