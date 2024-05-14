8d7a3d7f03168438d9a439c880bb94ef4a15781d
Omar Mohamed Yousef <mourra24@gmail.com>
2024-05-14 19:51:10 +0300

    Update sockets.py

--- sockets.py
+++ sockets.py
@@ -2,18 +2,28 @@ import socketio
 
 sio = socketio.Client()
 
+
 @sio.event
 def connect():
     print('connection established')
+
 
 @sio.event
 def my_message(data):
     print('message received with ', data)
     sio.emit('my response', {'response': 'my response'})
 
+
 @sio.event
 def disconnect():
     print('disconnected from server')
 
+
+@sio.event
+def login(data):
+    print(data)
+
+
 sio.connect('http://localhost:3000')
+sio.emit('login', {'username': 'Omar', 'password': '01060120066'})
 sio.wait()
